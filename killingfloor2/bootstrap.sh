5142741bbdf28263a354cc529c34191cdbbf1a40
Chris Rodriguez <damitchris@gmail.com>
2018-06-16 20:50:45 -0500

    fixed killing floor2
    auto installs kf2 server

--- killingfloor2/bootstrap.sh
+++ killingfloor2/bootstrap.sh
@@ -4,18 +4,19 @@
 myextra ()
 {
 	yum install epel-release -y;
-	yum install htop fail2ban;
+	yum install htop fail2ban vim nano;
 	timedatectl set-timezone America/Chicago
 	echo "alias ll='ls -lhp --color=auto'" >> /home/vagrant/.bashrc
 }
 
 killingfloor()
 {
-bash linuxgsm.sh kf2server
+su vagrant - -c "bash linuxgsm.sh kf2server"
+su vagrant - -c "bash kf2server auto-install"
 }
 
 #yum clean all
-yum update -y
+#yum update -y
 #install dep packages for 64 bit
 yum install -y mailx postfix curl wget bzip2 gzip unzip python binutils bc tmux glibc.i686 libstdc++ libstdc++.i686
 #install my extra packages
@@ -24,7 +25,7 @@ myextra
 mkdir /home/vagrant/lgsm-share
 #Download linuxgsm script
 wget https://linuxgsm.com/dl/linuxgsm.sh && chmod +x linuxgsm.sh
-#Install Killing Floor
-killingfloor
+#Install Killing Floor 2
+#killingfloor
 #Set hostname
 hostnamectl set-hostname lgsm-server
